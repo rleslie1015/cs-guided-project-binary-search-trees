91d3ffe499ea875c13ee1ac237bd83429f87ec10
Leslie Rodriguez <rleslie1015@gmail.com>
2020-11-10 15:08:52 -0600

    finished codesignal test, updates files for notes

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,22 +1,24 @@
 """
 You are given a binary tree.
-
+​
 Write a function that can find the **maximum depth** of the binary tree. The
 maximum depth can be defined as the number of nodes found along the longest
 path from the root down to the furthest leaf node. Remember, a leaf node is a
 node that has no children.
-
+​
 Example:
-
+​
 Given the following binary tree
-
+​
     5
    / \
   12  32
      /  \
     8    4
-
-your function should return the depth = 3.
+   /
+  11 
+​
+your function should return the depth = 4.
 """
 class BinaryTreeNode:
     def __init__(self, value=0, left=None, right=None):
@@ -24,6 +26,15 @@ class BinaryTreeNode:
         self.left = left
         self.right = right
 
-def maxDepth(self, root):
+def maxDepth(root):
     # Your code here
-
+    # base case(s) 
+    # if we have an empty tree, return 0 
+    if root is None:
+        return 0 
+    # how do we get closer to a base case? 
+    # we recurse to the left and right 
+    left_height = maxDepth(root.left) + 1
+    right_height = maxDepth(root.right) + 1 
+    
+    return max(left_height, right_height)
